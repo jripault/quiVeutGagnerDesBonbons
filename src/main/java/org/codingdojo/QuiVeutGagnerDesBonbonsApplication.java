be5efe4a955e76d0a43a878b852c263deec1be9f
[JVI] <julien.villanti@worldline.com>
2016-12-22 09:50:54 +0100

    Init project

---
+++ src/main/java/org/codingdojo/QuiVeutGagnerDesBonbonsApplication.java
@@ -0,0 +1,12 @@
+package org.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiVeutGagnerDesBonbonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiVeutGagnerDesBonbonsApplication.class, args);
+    }
+}
